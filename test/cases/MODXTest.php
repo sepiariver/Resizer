781a3998e4811225746af84f5d56c257e4889be4
sepiariver <yj+github@paywith.com>
2019-09-11 10:27:27 -0700

    resizer instantiation failing test

--- test/cases/MODXTest.php
+++ test/cases/MODXTest.php
@@ -4,16 +4,21 @@ use PHPUnit\Framework\TestCase;
 class MODXTest extends TestCase 
 {
     protected $modx;
+    protected $res;
     
     protected function setUp(): void
     {
         require(dirname(dirname(__FILE__)) . '/config.core.php');
         require(MODX_CORE_PATH . "model/modx/modx.class.php");
         $this->modx = new modX();
+
+        require(dirname(dirname(dirname(__FILE__))) . '/core/components/resizer/model/resizer.class.php');
+        $this->resizer = new Resizer($this->modx, 2);
     }
     public function testConfig()
     {
         $this->assertSame(MODX_CONFIG_KEY, 'config');
         $this->assertTrue($this->modx instanceof modX);
+        $this->assertTrue($this->resizer instanceof Resizer);
     }
 }
